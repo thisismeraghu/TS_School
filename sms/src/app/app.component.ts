18bb374a69255e11f6ac0b06fe041b53deb3db6c
Raghu <raghuisavailable@outlook.com>
2024-12-21 11:55:35 +0530

    feat: created logo component

--- sms/src/app/app.component.ts
+++ sms/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LogoComponent } from './logo/logo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LogoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
