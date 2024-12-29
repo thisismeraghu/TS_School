96bd86ab8fb395fb5a9eb924a5bb3c098552690b
BHARATH GUBBI SESHADRI <bharathgubbi43@gmail.com>
2024-12-29 01:14:12 -0800

    Adding Class to Navigation Component.

--- sms/src/app/navigation-bar/navigation-bar.component.ts
+++ sms/src/app/navigation-bar/navigation-bar.component.ts
@@ -7,5 +7,51 @@ import { Component } from '@angular/core';
   styleUrl: './navigation-bar.component.scss'
 })
 export class NavigationBarComponent {
+  executionMethod(){
+  var Manufacturer1 = new Manufacturer()
+  Manufacturer1.ManufactureCar()  
+}
+
+  
+}
+
+class Car {
+Name:string = '';
+Wheel:string = ''
+
+move(){
+console.log('car is moving.')
+}
+
+constructor(carname:string,carwheel:string){
+  console.log('Car Class Start.')
+  this.Name = carname
+  this.Wheel = carwheel
+}
 
 }
+
+class bmw extends Car{
+  awd:boolean = false;
+  color:string = ''
+
+  constructor(bmwcarname:string,bmwwheel:string,bmwcolor:string,bmwawd:boolean){
+    console.log('bmw Class Start.')
+    super(bmwcarname,bmwwheel)
+    this.awd = bmwawd
+    this.color = bmwcolor
+
+  }
+
+
+
+}
+
+class Manufacturer{
+
+  ManufactureCar(){
+    console.log('Manufacturer Class Start.')
+    var bmw1 = new bmw('x1','32','white',true)
+  }
+}
+
